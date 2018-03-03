2b2d9927c75a4e75d153c0d1cc7fcc2e25f0b430
Kaffemakarn <micsod@student.chalmers.se>
2018-03-03 14:15:29 +0100

    Added ExceptionHandler

---
+++ src/main/java/edu/chl/recipebok/util/ExceptionHandler.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.recipebok.util;
+
+/**
+ *
+ * @author Mickaela
+ */
+public class ExceptionHandler {
+
+    public static String getMessage(Throwable re) {
+        while (re.getCause() != null) {
+            re = re.getCause();
+            return re.getMessage();
+        }
+        return "Unknown";
+    }
+}
+
