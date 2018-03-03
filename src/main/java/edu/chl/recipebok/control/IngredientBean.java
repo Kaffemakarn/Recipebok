616bd0cdde833422f0d32fb88caa38fbfe5f4a41
daRaha <da_raha@hotmail.com>
2018-03-03 13:25:32 +0100

    added methods

--- src/main/java/edu/chl/recipebok/control/IngredientBean.java
+++ src/main/java/edu/chl/recipebok/control/IngredientBean.java
@@ -5,6 +5,7 @@
  */
 package edu.chl.recipebok.control;
 import edu.chl.recipebok.core.Cookbook;
+import edu.chl.recipebok.core.Ingredient;
 import edu.chl.recipebok.core.Recipe;
 import edu.chl.recipebok.dao.IngredientCatalogue;
 import edu.chl.recipebok.dao.RecipeCatalogue;
@@ -32,12 +33,12 @@ import net.bootsfaces.utils.FacesMessages;
 @SessionScoped
 
 public class IngredientBean implements Serializable{
- private static final Logger LOG = Logger.getLogger(CookbookBean.class.getName());
+ private static final Logger LOG = Logger.getLogger(IngredientBean.class.getName());
     @EJB
     private IngredientCatalogue ibCat;
     @Getter
     @Setter
-    private Cookbook tmp = new Cookbook();
+    private Ingredient tmp = new Ingredient();
     private final int start = 0;
     private int nRecords = 50;
 
@@ -45,7 +46,43 @@ public class IngredientBean implements Serializable{
     @Setter
     private Form form;
    
+      public void page() {
+        /*Map<String, String> map = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        for( Entry<String, String> e : map.entrySet()){
+        LOG.log(Level.INFO, "Key " + e.getKey() + " val " + e.getValue() );
+        }*/
+       DataTable dt = (DataTable) FacesContext.getCurrentInstance().getViewRoot().findComponent("authorForm:authorTable");
+       
+       LOG.log(Level.INFO, "Test {0}", dt.getJQueryEvents()); //) +  );
+    }
+      
+       // ------------ Navigation -------------------
+
+    public void cancel() {
+        tmp = new Ingredient();
+    }
     
-    
+  // --------- Call backend -------------------------
+    public void setIngredient() {
+        tmp = ibCat.find(tmp.getname);
+    }
+
+   
+     public void add() {
+        /*tmp.setAddress(DataSupplier.getRandomAddress());
+        try {
+            rcat.create(tmp);
+            FacesMessages.info("Success");
+        } catch (RuntimeException sql) {
+            String message = ExceptionHandler.getMessage(sql);
+            FacesMessages.info("Fail " + message);
+        }
+        tmp = new Recipe();*/
+    }
+
+    public void update() {
+        ibCat.update(tmp);
+        tmp = new Ingredient();
+    }
     
 }
