9b378a1539419481c581c8e15dc3524d31032cee
daRaha <da_raha@hotmail.com>
2018-03-03 12:09:42 +0100

    IngredientBean

--- src/main/java/edu/chl/recipebok/control/IngredientBean.java
+++ src/main/java/edu/chl/recipebok/control/IngredientBean.java
@@ -4,11 +4,48 @@
  * and open the template in the editor.
  */
 package edu.chl.recipebok.control;
-
+import edu.chl.recipebok.core.Cookbook;
+import edu.chl.recipebok.core.Recipe;
+import edu.chl.recipebok.dao.IngredientCatalogue;
+import edu.chl.recipebok.dao.RecipeCatalogue;
+import java.io.Serializable;
+import static java.lang.System.out;
+import java.text.Normalizer.Form;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import lombok.*;
+import net.bootsfaces.component.dataTable.DataTable;
+import net.bootsfaces.utils.FacesMessages;
+/**
 /**
  *
  * @author rahadadgar
  */
-public class IngredientBean {
+@Named("ingredientbean")
+//@RequestScoped
+@SessionScoped
+
+public class IngredientBean implements Serializable{
+ private static final Logger LOG = Logger.getLogger(CookbookBean.class.getName());
+    @EJB
+    private IngredientCatalogue ibCat;
+    @Getter
+    @Setter
+    private Cookbook tmp = new Cookbook();
+    private final int start = 0;
+    private int nRecords = 50;
+
+    @Getter
+    @Setter
+    private Form form;
+   
+    
+    
     
 }
