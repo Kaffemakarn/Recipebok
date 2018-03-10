99608cd4cdf853363dd18a6668ba316d14cfcd13
August Lennar <lennar@student.chalmers.se>
2018-03-10 16:44:37 +0100

    Implemented first working draft of Image classes

--- src/main/java/edu/chl/recipebok/dao/ImageCatalogue.java
+++ src/main/java/edu/chl/recipebok/dao/ImageCatalogue.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.chl.recipebok.dao;
+
+import edu.chl.recipebok.core.Image;
+import edu.chl.recipebok.core.QImage;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
- * @author aug
+ * @author August
  */
-public class ImageCatalogue {
+@Stateless
+public class ImageCatalogue extends AbstractQuery<Image, Long> {
     
+    @PersistenceContext(unitName = "recipebok_pu")
+    private EntityManager em;
+      
+
+    public ImageCatalogue() {
+        super(Image.class, QImage.image);
+    }
+
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+    
+    @Override
+    public void setEntityManager(EntityManager em) {
+        this.em = em;
+    }
 }
